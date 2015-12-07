3ab3a6027ca88036de249b5efdf7e8f3ceb47f1f
Michael Schlies <michael.schlies@target.com>
2015-12-07 09:48:54 -0600

    Added support for handling service restarts per the previous flow of the recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,9 @@
 #
 # Copyright 2015, Mindera
 #
-
 if node['carbon-relay-ng']['supervisor']['enable']
   include_recipe 'supervisor'
+  svc = 'supervisor_service[carbon-relay-ng]'
 elsif node['init_package'].equal?('systemd')
   template '/etc/systemd/system/carbon.service' do
     source 'systemd.erb'
@@ -33,16 +33,16 @@ GO_OWNER = node['go']['owner']
 GO_GROUP = node['go']['group']
 
 [
-  'go get -u github.com/jteeuwen/go-bindata/...',
-  'go get -u -d github.com/graphite-ng/carbon-relay-ng/...'
+    'go get -u github.com/jteeuwen/go-bindata/...',
+    'go get -u -d github.com/graphite-ng/carbon-relay-ng/...'
 ].each do |cmd|
   execute cmd do
     user GO_OWNER
     group GO_GROUP
     environment(
-      'GOPATH' => GO_PATH,
-      'GOBIN' => GO_BIN,
-      'PATH' => PATH
+        'GOPATH' => GO_PATH,
+        'GOBIN' => GO_BIN,
+        'PATH' => PATH
     )
   end
 end
@@ -52,9 +52,9 @@ execute 'make install' do
   group GO_GROUP
   cwd "#{GO_PATH}/src/github.com/graphite-ng/carbon-relay-ng"
   environment(
-    'GOPATH' => GO_PATH,
-    'GOBIN' => GO_BIN,
-    'PATH' => PATH
+      'GOPATH' => GO_PATH,
+      'GOBIN' => GO_BIN,
+      'PATH' => PATH
   )
   not_if { ::File.exist?("#{GO_BIN}/carbon-relay-ng") }
 end
@@ -85,7 +85,7 @@ end
 template "#{CARBON_CONF_DIR}/carbon-relay-ng.ini" do
   source 'carbon-relay-ng.ini.erb'
   backup false
-  notifies :restart, 'supervisor_service[carbon-relay-ng]', :delayed
+  notifies :restart, svc, :delayed
 end
 
 SUP_PROC_NAME = node['carbon-relay-ng']['supervisor']['process_name']
@@ -93,9 +93,15 @@ SUP_STDOUT_LOG = node['carbon-relay-ng']['supervisor']['stdout_logfile']
 SUP_STDERR_LOG = node['carbon-relay-ng']['supervisor']['stderr_logfile']
 
 # TODO: add extra supervisor service configs
-supervisor_service 'carbon-relay-ng' do
-  command "#{GO_BIN}/carbon-relay-ng #{CARBON_CONF_DIR}/carbon-relay-ng.ini"
-  process_name SUP_PROC_NAME
-  stdout_logfile "#{CARBON_LOG_DIR}/#{SUP_STDOUT_LOG}"
-  stderr_logfile "#{CARBON_LOG_DIR}/#{SUP_STDERR_LOG}"
+if node['carbon-relay-ng']['supervisor']['enable']
+  supervisor_service 'carbon-relay-ng' do
+    command "#{GO_BIN}/carbon-relay-ng #{CARBON_CONF_DIR}/carbon-relay-ng.ini"
+    process_name SUP_PROC_NAME
+    stdout_logfile "#{CARBON_LOG_DIR}/#{SUP_STDOUT_LOG}"
+    stderr_logfile "#{CARBON_LOG_DIR}/#{SUP_STDERR_LOG}"
+  end
+else
+  service 'carbon' do
+    action :nothing
+  end
 end
