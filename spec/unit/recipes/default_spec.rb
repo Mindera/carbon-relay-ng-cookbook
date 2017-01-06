0b1e234f1a87e97e39cd465da688d7bbc8ff81fc
ptQa <ptqa.mail@gmail.com>
2017-01-06 21:36:21 +0100

    no golang sources

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -4,28 +4,8 @@ describe 'carbon-relay-ng::default' do
   cached(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
   cached(:node) { chef_run.node }
 
-  before do
-    stub_command("/usr/local/go/bin/go version | grep \"go1.4 \"").and_return(true)
-  end
-
   it 'include the supervisor recipe' do
     expect(chef_run).to include_recipe('supervisor')
-  end
-
-  it 'include the golang::packages recipe' do
-    expect(chef_run).to include_recipe('golang')
-  end
-
-  it 'install the go-bindata binary' do
-    expect(chef_run).to run_execute('go get -u github.com/jteeuwen/go-bindata/...')
-  end
-
-  it 'gets the carbon-relay-ng source' do
-    expect(chef_run).to run_execute('go get -u -d github.com/graphite-ng/carbon-relay-ng/...')
-  end
-
-  it 'install the carbon-relay-ng binary' do
-    expect(chef_run).to run_execute('make install')
   end
 
   it 'creates the spool directory' do
